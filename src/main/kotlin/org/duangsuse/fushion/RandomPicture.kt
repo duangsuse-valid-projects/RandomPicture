ddb7f007778d74c94e5246d73183cee56022bccb
duangsuse <fedora-opensuse@outlook.com>
2019-04-01 10:02:01 +0800

    Server: fix Random index, add endpoints
    - Now last index will not be ignored
    - Added: getById, getNameById, getList

--- src/main/kotlin/org/duangsuse/fushion/RandomPicture.kt
+++ src/main/kotlin/org/duangsuse/fushion/RandomPicture.kt
@@ -1,6 +1,8 @@
 package org.duangsuse.fushion
 
 import java.io.*
+import java.util.*
+
 import kotlin.random.Random
 
 import io.vertx.core.*
@@ -14,7 +16,15 @@ typealias Path = String
 
 //// Helper functions
 fun <A, B> Future<A>.catching(chain: Future<B>): Future<A> = this.setHandler { if (this.succeeded()) chain.complete() else chain.fail(this.cause()) }
-fun <T> List<T>.sample(): T = this[Random.nextInt(this.lastIndex)]
+fun <T> List<T>.indexedSample(): Pair<Int, T> = Random.nextInt(this.size).let { Pair(it, this[it]) }
+
+val String.java get() = this as java.lang.String
+fun java.lang.String.toUrlBase64(): String = Base64.getUrlEncoder().encodeToString(this.getBytes())
+
+fun HttpServerResponse.putHeader(h: CharSequence, o: Number) = run { putHeader(h, o.toString()) }
+
+fun HttpServerResponse.contentTypeUtf8() = run { putHeader(HttpHeaders.CONTENT_TYPE, "text/plain;charset=utf8") }
+fun HttpServerResponse.writeEnd(body: String) = run { putHeader(HttpHeaders.CONTENT_LENGTH, body.java.getBytes().size); write(body); end() }
 
 /**
  * Random picture service
@@ -65,23 +75,49 @@ class RandomPicture: AbstractVerticle() {
     }
 
     //// Serveice HTTP Server abstraction
-    private fun createHttpServer(router: Router): HttpServer {
-        val httpSrv = vertx.createHttpServer()
+    private fun createHttpServer(router: Router): HttpServer = vertx.createHttpServer().requestHandler(router)
 
-        httpSrv.requestHandler(router)
-        return httpSrv
-    }
-
-    fun randomImage(): Path = pictures.sample()
+    fun randomImage(): Pair<Int, Path> = pictures.indexedSample()
 
     private fun drawRoutes(): Router {
         val router = Router.router(vertx)
 
         router.route(HttpMethod.GET, IMAGE_SERVICE_PATH).handler {
+            val (index, path) = randomImage()
             it.response()
                 .setStatusCode(200)
                 .putHeader(HttpHeaders.CACHE_CONTROL, "no-cache")
-                .sendFile(randomImage())
+                .putHeader(INDEX_HEADER, index.toString())
+                .putHeader(FILENAME_HEADER, path.java.toUrlBase64())
+                .sendFile(path)
+        }
+
+        /// External
+        router.get(IMAGE_LIST_SERVICE_PATH).handler {
+            it.response()
+                .contentTypeUtf8()
+                .writeEnd(pictures.indices.zip(pictures).joinToString("\n"))
+        }
+        router.get(INDEX_SERVICE_PATH).handler {
+            val index = Helper.httpIdParamOrNull(it)
+            index?.let(pictures::getOrNull)?.run {
+                it.response()
+                    .putHeader(INDEX_HEADER, index.toString())
+                    .sendFile(this)
+
+                return@handler
+            }
+            Helper.badIndexResponse(it.response())
+        }
+        router.get(INDEX_NAME_SERIVCE_PATH).handler {
+            val index = Helper.httpIdParamOrNull(it)
+            index?.let(pictures::getOrNull)?.run {
+                it.response()
+                    .contentTypeUtf8()
+                    .writeEnd(this)
+                return@handler
+            }
+            Helper.badIndexResponse(it.response())
         }
 
         return router
@@ -136,14 +172,25 @@ class RandomPicture: AbstractVerticle() {
         const val ENV_PORT = "PORT"
         const val IMAGE_SERVICE_PATH = "/"
 
+        const val IMAGE_LIST_SERVICE_PATH = "/list"
+        const val INDEX_SERVICE_PATH = "/:id"
+        const val INDEX_NAME_SERIVCE_PATH = "/:id/name"
+
+        const val BAD_INDEX_MESSAGE = "Index not found or not integer"
+
+        const val FILENAME_HEADER = "Image-File-Name"
+        const val INDEX_HEADER = "Image-No"
+
         private const val ACCEPTABLE_EXTENSIONS = "png,jpg,jpeg,gif,webp,raw,bmp,img,svg"
-        val ACCEPTABLE_REGEX = ACCEPTABLE_EXTENSIONS.split(',').map("\\."::plus).joinToString("|").let { "^.*($it)$" }.let(::Regex)
+        val ACCEPTABLE_REGEX = ACCEPTABLE_EXTENSIONS.split(',').joinToString("|").let { "^.*\\.($it)$" }.let(::Regex)
     }
 
     object Helper {
         internal fun env(name: String): String? = System.getenv(name)
         internal fun isImageFile(f: File) = ACCEPTABLE_REGEX.matches(f.name)
         internal fun timeTicks(): Long = System.currentTimeMillis()
+        internal fun badIndexResponse(stream: HttpServerResponse) = stream.setStatusCode(400).writeEnd(BAD_INDEX_MESSAGE)
+        internal fun httpIdParamOrNull(endpoint: RoutingContext): Int? = endpoint.request().getParam("id").toIntOrNull();
     }
 }
 
